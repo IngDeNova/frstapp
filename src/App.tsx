dae69cfdc66c9219c9873af4e3a122c02174b4e3
IngDeNova <155915080+IngDeNova@users.noreply.github.com>
2024-10-01 15:05:24 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectManagementApp from './ProjectManagementApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectManagementApp />
+    </div>
+  );
+}
+
+export default App;
